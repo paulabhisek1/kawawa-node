751c1c2c4466e1899aea8c3032cd17d8a3308c33
Abhisek <paulabhisek1@gmail.com>
2021-02-12 01:19:33 +0530

    update

--- validation-schemas/UsersValidationSchemas.js
+++ validation-schemas/UsersValidationSchemas.js
@@ -7,7 +7,7 @@ module.exports.userRegisterSchema = Joi.object().keys({
     full_name: Joi.string().required(),
     email: Joi.string().email().required(),
     mobile_no: Joi.string().required(),
-    password: Joi.string().required().min(6).max(25).pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*(){}:;_+-/=<>,.?|`~"\']+$')).messages({
+    password: Joi.string().required().min(6).max(25).messages({
         'string.pattern.base': "Your password must be at least 8 characters long, contain at least one number and have a mixture of uppercase and lowercase letters",
     }), //password must include at least 1 upper case letter, 1 lower case letter, and 1 numeric digit
     confirm_password: Joi.string().equal(Joi.ref('password')).required().messages({
@@ -47,7 +47,7 @@ module.exports.otpVerificationSchema = Joi.object().keys({
 
 // Reset Password Schema
 module.exports.resetPassSchema = Joi.object().keys({
-    password: Joi.string().required().min(8).max(25).pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*(){}:;_+-/=<>,.?|`~"\']+$')).messages({
+    password: Joi.string().required().min(6).max(25).messages({
         'string.pattern.base': "Your password must be at least 8 characters long, contain at least one number and have a mixture of uppercase and lowercase letters",
     }), //password must include at least 1 upper case letter, 1 lower case letter, and 1 numeric digit
     confirm_password: Joi.string().equal(Joi.ref('password')).required().messages({
