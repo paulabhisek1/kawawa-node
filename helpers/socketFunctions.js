305b9340114a4ada0c73c40e387542dab12104ff
Abhisek <paulabhisek1@gmail.com>
2021-05-05 23:10:20 +0530

    update

--- helpers/socketFunctions.js
+++ helpers/socketFunctions.js
@@ -8,47 +8,52 @@ const jwtOptionsAccess = global.constants.jwtAccessTokenOptions;
 module.exports.socketResponse = (socket) => {
 
     // Connection Tester
-    socket.on('test-socket', (data)=>{
+    socket.on('test-socket', (data) => {
         console.log("Socket Configured Successfully...");
     });
 
     // Save Song Details
-    socket.on('user-played', async (data, callback)=>{
+    socket.on('user-played', async(data, callback) => {
         let songID = data.songID;
         let accessToken = data.accessToken.split(' ')[1];
-        console.log("TOKEN : ", accessToken);
-        try{
-            jwt.verify(accessToken, jwtOptionsAccess.secret, async (err, decodedToken) => {
+        // console.log("TOKEN : ", accessToken);
+        try {
+            jwt.verify(accessToken, jwtOptionsAccess.secret, async(err, decodedToken) => {
                 console.log("ERROR : ", err);
                 if (err) {
                     callback({
                         status: 0,
                         msg: responseMessages.authFailure,
-                    }) 
-                }
-                else {
+                    })
+                } else {
                     let userCount = await userRepositories.count({ where: { id: decodedToken.user_id } });
-                    
-                    if(userCount > 0) {
+
+                    if (userCount > 0) {
                         let userID = decodedToken.user_id;
                         let songCount = await songRepositories.count({ id: songID, is_active: 1 });
-    
-                        if(songCount > 0) {
+
+                        if (songCount > 0) {
                             await userPlayedHistoryRepositories.update({ user_id: userID, file_id: songID }, { updatedAt: Date.now() });
-    
+
                             callback({
                                 status: 1,
                                 msg: `User Played History Updated`,
                             })
-                        }
-                        else{
+                        } else {
+
+                            await userPlayedHistoryRepositories.create({ user_id: userID, file_id: songID, type: 'song' });
+
                             callback({
-                                status: 0,
-                                msg: `Invalid Song ID Provided`,
+                                status: 1,
+                                msg: `User Played History Added`,
                             })
+
+                            // callback({
+                            //     status: 0,
+                            //     msg: `Invalid Song ID Provided`,
+                            // })
                         }
-                    }
-                    else{
+                    } else {
                         callback({
                             status: 0,
                             msg: responseMessages.authFailure,
@@ -56,8 +61,7 @@ module.exports.socketResponse = (socket) => {
                     }
                 }
             })
-        }
-        catch(err) {
+        } catch (err) {
             callback({
                 status: 0,
                 msg: responseMessages.serverError,
@@ -66,43 +70,40 @@ module.exports.socketResponse = (socket) => {
     });
 
     // Update Song Played Count
-    socket.on('song-played', async (data, callback)=>{
+    socket.on('song-played', async(data, callback) => {
         let songID = data.songID;
         let accessToken = data.accessToken.split(' ')[1];
-        try{
-            jwt.verify(accessToken, jwtOptionsAccess.secret, async (err, decodedToken) => {
+        try {
+            jwt.verify(accessToken, jwtOptionsAccess.secret, async(err, decodedToken) => {
                 if (err) {
                     callback({
                         status: 0,
                         msg: responseMessages.authFailure,
-                    }) 
-                }
-                else {
+                    })
+                } else {
                     let userCount = await userRepositories.count({ where: { id: decodedToken.user_id } });
-                    
-                    if(userCount > 0) {
+
+                    if (userCount > 0) {
                         let songDet = await songRepositories.findOne({ id: songID, is_active: 1 });
-    
-                        if(songDet) {
+
+                        if (songDet) {
                             let updateData = {};
-                            if(songDet.playedCount) updateData.playedCount = songDet.playedCount + 1;
+                            if (songDet.playedCount) updateData.playedCount = songDet.playedCount + 1;
                             else updateData.playedCount = 1;
 
                             await songRepositories.update({ id: songID }, updateData)
-    
+
                             callback({
                                 status: 1,
                                 msg: `Song Played Count Updated`,
                             })
-                        }
-                        else{
+                        } else {
                             callback({
                                 status: 0,
                                 msg: `Invalid Song ID Provided`,
                             })
                         }
-                    }
-                    else{
+                    } else {
                         callback({
                             status: 0,
                             msg: responseMessages.authFailure,
@@ -110,8 +111,7 @@ module.exports.socketResponse = (socket) => {
                     }
                 }
             })
-        }
-        catch(err) {
+        } catch (err) {
             console.log("ERR : ", err);
             callback({
                 status: 0,
